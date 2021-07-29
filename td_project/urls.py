9157f55558f14772db62ba3493cc801d42ebf3b2
Peng Zhao <2602594z@student.gla.ac.uk>
2021-07-29 17:49:20 +0100

    second commit

--- td_project/urls.py
+++ td_project/urls.py
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from django.contrib import admin
+from django.conf.urls import include 
+from rango import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^$', views.index, name='index'),
+    url(r'^rango/', include('rango.urls')),
+    # above maps any URLs starting 
+    # with rango/ to be handled by
+    # the rango application
+    url(r'^admin/', admin.site.urls),
 ]
