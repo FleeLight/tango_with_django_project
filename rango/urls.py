a1cc04e5f0c53ad42ade43d680a0d6668ff6cc96
Peng Zhao <2602594z@student.gla.ac.uk>
2021-07-29 21:00:07 +0100

    5th commit

--- rango/urls.py
+++ rango/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import url
+from django.conf.urls import include 
+from django.urls import path
 from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-]
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- td_project/settings.py
+++ td_project/settings.py
@@ -16,6 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = [STATIC_DIR, ]
 
@@ -66,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media'
             ],
         },
     },
@@ -121,3 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_URL = '/static/'
+
+
+# Media files
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
