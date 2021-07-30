8697e5cdf5ea9dba19b856574d88db32a2840b31
Peng Zhao <2602594z@student.gla.ac.uk>
2021-07-30 13:19:07 +0100

    12th commit

--- rango/models.py
+++ rango/models.py
@@ -24,5 +24,16 @@ class Page(models.Model):
     url = models.URLField()
     views = models.IntegerField(default=0)
 
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+        # If url is not empty and doesn't start with 'http://',
+        # then prepend 'http://'.
+        if url and not url.startswith('http://'):
+            url = f'http://{url}'
+            cleaned_data['url'] = url
+
+        return cleaned_data
+
     def __str__(self):  # For Python 2, use __unicode__ too
         return self.title
