6dc877ad77ec68e9b35f7fbb1883ce05cede798f
Peng Zhao <2602594z@student.gla.ac.uk>
2021-07-30 15:02:04 +0100

    16th commit

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from rango.models import Category, Page
+from rango.models import UserProfile
 
 # Add in this class to customise the Admin Interface
 class CategoryAdmin(admin.ModelAdmin):
@@ -7,3 +8,4 @@ class CategoryAdmin(admin.ModelAdmin):
 
 # Update the registration to include this customised interface
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(UserProfile)
