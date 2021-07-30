7d27b981eb078997c17091a58a1451a524953d0b
Peng Zhao <2602594z@student.gla.ac.uk>
2021-07-30 14:11:31 +0100

    14th commit

--- rango/urls.py
+++ rango/urls.py
@@ -4,6 +4,8 @@ from django.urls import path
 from rango import views
 from django.conf import settings
 
+app_name = 'rango'
+
 urlpatterns = [
         path('', views.index, name='index'),
         path('about/', views.about, name='about'),
